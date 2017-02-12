a29ddb2c8f93f5ef5a5cf0f29c99efc003de125f
Simon <labute.simon@gmail.com>
2017-02-12 18:34:32 -0500

    Fix CREATE TABLE statements.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,51 +9,51 @@ class Main {
 		SchemaDefinition.outputCreateSqlScript("outputs/createScript.sql", "simon_");
 		SchemaDefinition.outputDropSqlScript("outputs/dropScript.sql", "simon_");
 		
-		//open db connection
-		System.out.println("Attempting to establish DB connection.");
-		DbConnection con = new DbConnection();
-		System.out.println("DB connection established.");
-		
-		//create the schema
-		String[] tables = SchemaDefinition.getTables("simon_");
-		String tableName = tables[0];
-		con.executeUpdate(tables);
-		String createSQL = "CREATE TABLE " + tableName + " (id INTEGER, name VARCHAR (25)) ";
-		con.execute(createSQL);
-
-		// Inserting Data into the table
-		String insertSQL = "INSERT INTO " + tableName + " VALUES ( 1 , \'Vicki\' ) ";
-		con.executeUpdate(insertSQL);
-
-		insertSQL = "INSERT INTO " + tableName + " VALUES ( 2 , \'Vera\' ) ";
-		con.executeUpdate(insertSQL);
-		insertSQL = "INSERT INTO " + tableName + " VALUES ( 3 , \'Franca\' ) ";
-		con.executeUpdate(insertSQL);
-
-
-		// Querying a table
-		String querySQL = "SELECT id, name from " + tableName + " WHERE NAME = \'Vicki\'";
-		ResultSet rs = con.executeQuery(querySQL);
-		try {
-			while (rs.next()) {
-				int id = rs.getInt(1);
-				String name = rs.getString(2);
-				System.out.println("id:  " + id);
-				System.out.println("name:  " + name);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		// Updating a table
-		String updateSQL = "UPDATE " + tableName + " SET NAME = \'Mimi\' WHERE id = 3";
-		con.executeUpdate(updateSQL);
-
-		// Dropping a table
-		String dropSQL = "DROP TABLE " + tableName;
-		con.executeUpdate(dropSQL);
-		
-		//close out connection
-		con.close();
+//		//open db connection
+//		System.out.println("Attempting to establish DB connection.");
+//		DbConnection con = new DbConnection();
+//		System.out.println("DB connection established.");
+//		
+//		//create the schema
+//		String[] tables = SchemaDefinition.getTables("simon_");
+//		String tableName = tables[0];
+//		con.executeUpdate(tables);
+//		String createSQL = "CREATE TABLE " + tableName + " (id INTEGER, name VARCHAR (25)) ";
+//		con.execute(createSQL);
+//
+//		// Inserting Data into the table
+//		String insertSQL = "INSERT INTO " + tableName + " VALUES ( 1 , \'Vicki\' ) ";
+//		con.executeUpdate(insertSQL);
+//
+//		insertSQL = "INSERT INTO " + tableName + " VALUES ( 2 , \'Vera\' ) ";
+//		con.executeUpdate(insertSQL);
+//		insertSQL = "INSERT INTO " + tableName + " VALUES ( 3 , \'Franca\' ) ";
+//		con.executeUpdate(insertSQL);
+//
+//
+//		// Querying a table
+//		String querySQL = "SELECT id, name from " + tableName + " WHERE NAME = \'Vicki\'";
+//		ResultSet rs = con.executeQuery(querySQL);
+//		try {
+//			while (rs.next()) {
+//				int id = rs.getInt(1);
+//				String name = rs.getString(2);
+//				System.out.println("id:  " + id);
+//				System.out.println("name:  " + name);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//
+//		// Updating a table
+//		String updateSQL = "UPDATE " + tableName + " SET NAME = \'Mimi\' WHERE id = 3";
+//		con.executeUpdate(updateSQL);
+//
+//		// Dropping a table
+//		String dropSQL = "DROP TABLE " + tableName;
+//		con.executeUpdate(dropSQL);
+//		
+//		//close out connection
+//		con.close();
 	}
 }
--- src/main/SchemaDefinition.java
+++ src/main/SchemaDefinition.java
@@ -12,88 +12,99 @@ public class SchemaDefinition {
 	 * @return the CREATE TABLE statements for the tables of the schema
 	 */
 	public static String[] getTables(String prefix){
+		//TODO validate prefix
+		
 		String[] tables = {
 //				"CREATE TABLE " + prefix + "example (id INTEGER, name VARCHAR (25))",
 				"CREATE TABLE " + prefix + "user ("
-						+ "user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),"
-						+ "username VARCHAR(50) UNIQUE,"
-						+ "name VARCHAR(50) NOT NULL,"
-						+ "password VARCHAR(191) NOT NULL,"
-						+ "role VARCHAR(10) NOT NULL,"
-						+ "CHECK (role='admin' OR role='user'),"
+						+ "user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "
+						+ "username VARCHAR(50) UNIQUE NOT NULL, "
+						+ "name VARCHAR(50) NOT NULL, "
+						+ "password VARCHAR(191) NOT NULL, "
+						+ "role VARCHAR(10) NOT NULL, "
+						+ "CHECK (role='admin' OR role='user') "
 					+ ")",
 				"CREATE TABLE " + prefix + "message ("
-						+ "message_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),"
-						+ "text VARCHAR(500) NOT NULL,"
-						+ "owner_id INTEGER NOT NULL FOREIGN KEY REFERENCES user (user_id),"
+						+ "message_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "
+						+ "text VARCHAR(500) NOT NULL, "
+						+ "owner_id INTEGER NOT NULL, "
+						+ "FOREIGN KEY (owner_id) REFERENCES " + prefix + "user (user_id) "
 					+ ")",
 				"CREATE TABLE " + prefix + "receives ("
-						+ "message_id INTEGER NOT NULL,"
-						+ "user_id INTEGER NOT NULL,"
-						+ "PRIMARY KEY (message_id, user_id),"
-						+ "FOREIGN KEY (message_id) REFERENCES message (message_id),"
-						+ "FOREIGN KEY (user_id) REFERENCES user (user_id),"
+						+ "message_id INTEGER NOT NULL, "
+						+ "user_id INTEGER NOT NULL, "
+						+ "PRIMARY KEY (message_id, user_id), "
+						+ "FOREIGN KEY (message_id) REFERENCES " + prefix + "message (message_id), "
+						+ "FOREIGN KEY (user_id) REFERENCES " + prefix + "user (user_id) "
 					+ ")",
 				"CREATE TABLE " + prefix + "alert ("
-						+ "message_id INTEGER PRIMARY KEY FOREIGN KEY REFERENCES message (message_id),"
-						+ "type VARCHAR(7) NOT NULL,"
-						+ "CHECK (type='success' OR type='info' OR type='warning' OR type='danger'),"
+						+ "message_id INTEGER NOT NULL PRIMARY KEY, "
+						+ "FOREIGN KEY (message_id) REFERENCES " + prefix + "message (message_id), "
+						+ "type VARCHAR(7) NOT NULL, "
+						+ "CHECK (type='success' OR type='info' OR type='warning' OR type='danger') "
 					+ ")",
 				"CREATE TABLE " + prefix + "notifies ("
-						+ "message_id INTEGER NOT NULL,"
-						+ "user_id INTEGER NOT NULL,"
-						+ "is_dismissed SMALLINT NOT NULL,"
-						+ "CHECK (is_dismissed=0 OR is_dismissed=1),"
-						+ "PRIMARY KEY (message_id, user_id),"
-						+ "FOREIGN KEY (message_id) REFERENCES message (message_id),"
-						+ "FOREIGN KEY (user_id) REFERENCES user (user_id),"
+						+ "message_id INTEGER NOT NULL, "
+						+ "user_id INTEGER NOT NULL, "
+						+ "is_dismissed SMALLINT NOT NULL, "
+						+ "CHECK (is_dismissed=0 OR is_dismissed=1), "
+						+ "PRIMARY KEY (message_id, user_id), "
+						+ "FOREIGN KEY (message_id) REFERENCES " + prefix + "message (message_id), "
+						+ "FOREIGN KEY (user_id) REFERENCES " + prefix + "user (user_id) "
 					+ ")",
 				"CREATE TABLE " + prefix + "page ("
-						+ "page_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),"
-						+ "title VARCHAR(200) NOT NULL,"
-						+ "is_public SMALLINT NOT NULL,"
-						+ "owner_id INTEGER NOT NULL FOREIGN KEY REFERENCES user (user_id),"
-						+ "CHECK (is_public=0 OR is_public=1),"
+						+ "page_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "
+						+ "title VARCHAR(200) NOT NULL, "
+						+ "is_public SMALLINT NOT NULL, "
+						+ "owner_id INTEGER NOT NULL, "
+						+ "FOREIGN KEY (owner_id) REFERENCES " + prefix + "user (user_id), "
+						+ "CHECK (is_public=0 OR is_public=1) "
 					+ ")",
 				"CREATE TABLE " + prefix + "viewable_by ("
-						+ "page_id INTEGER NOT NULL,"
-						+ "user_id INTEGER NOT NULL,"
-						+ "PRIMARY KEY (page_id, user_id),"
-						+ "FOREIGN KEY page_id REFERENCES page (page_id),"
-						+ "FOREIGN KEY user_id REFERENCES user (user_id),"
+						+ "page_id INTEGER NOT NULL, "
+						+ "user_id INTEGER NOT NULL, "
+						+ "PRIMARY KEY (page_id, user_id), "
+						+ "FOREIGN KEY (page_id) REFERENCES " + prefix + "page (page_id), "
+						+ "FOREIGN KEY (user_id) REFERENCES " + prefix + "user (user_id) "
 					+ ")",
 				"CREATE TABLE " + prefix + "comment ("
-						+ "comment_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),"
-						+ "text VARCHAR(200) NOT NULL,"
-						+ "page_id NOT NULL FOREIGN KEY REFERENCES page (page_id),"
-						+ "user_id NOT NULL FOREIGN KEY REFERENCES user (user_id),"
+						+ "comment_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "
+						+ "text VARCHAR(200) NOT NULL, "
+						+ "page_id INTEGER NOT NULL, "
+						+ "FOREIGN KEY (page_id) REFERENCES " + prefix + "page (page_id), "
+						+ "user_id INTEGER NOT NULL, "
+						+ "FOREIGN KEY (user_id) REFERENCES " + prefix + "user (user_id) "
 					+ ")",
 				"CREATE TABLE " + prefix + "menu_item ("
-						+ "menu_item_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),"
-						+ "text VARCHAR(200) NOT NULL,"
-						+ "url VARCHAR(200) NOT NULL,"
+						+ "menu_item_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "
+						+ "text VARCHAR(200) NOT NULL, "
+						+ "url VARCHAR(200) NOT NULL "
 					+ ")",
 				"CREATE TABLE " + prefix + "menu_item_configuration ("
-						+ "config_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),"
-						+ "padding INTEGER NOT NULL,"
-						+ "background_colour VARCHAR(20) NOT NULL,"
-						+ "text_colour VARCHAR(20) NOT NULL,"
+						+ "config_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "
+						+ "padding INTEGER NOT NULL, "
+						+ "background_colour VARCHAR(20) NOT NULL, "
+						+ "text_colour VARCHAR(20) NOT NULL "
 					+ ")",
 				"CREATE TABLE " + prefix + "page_menu ("
-						+ "page_id INTEGER FOREIGN KEY REFERENCES page (page_id),"
-						+ "menu_item_id INTEGER FOREIGN KEY REFERENCES menu_item (menu_item_id),"
-						+ "config_id INTEGER FOREIGN KEY REFERENCES menu_item_configuration (config_id),"
-						+ "index INTEGER NOT NULL,"
-						+ "PRIMARY KEY (page_id, menu_item_id, config_id),"
+						+ "page_id INTEGER NOT NULL, "
+						+ "FOREIGN KEY (page_id) REFERENCES " + prefix + "page (page_id), "
+						+ "menu_item_id INTEGER NOT NULL, "
+						+ "FOREIGN KEY (menu_item_id) REFERENCES " + prefix + "menu_item (menu_item_id), "
+						+ "config_id INTEGER NOT NULL, "
+						+ "FOREIGN KEY (config_id) REFERENCES " + prefix + "menu_item_configuration (config_id), "
+						+ "index INTEGER NOT NULL, "
+						+ "PRIMARY KEY (page_id, menu_item_id, config_id) "
 					+ ")",
 				"CREATE TABLE " + prefix + "content ("
-						+ "page_id INTEGER FOREIGN KEY REFERENCES page (page_id),"
-						+ "index INTEGER NOT NULL,"
-						+ "styling VARCHAR(7) NOT NULL,"
-						+ "type VARCHAR(7) NOT NULL,"
-						+ "content CLOB NOT NULL,"
-						+ "PRIMARY KEY (page_id, index),"
-						+ "CHECK (styling='airy' OR styling='compact'),"
+						+ "page_id INTEGER NOT NULL, "
+						+ "FOREIGN KEY (page_id) REFERENCES " + prefix + "page (page_id), "
+						+ "index INTEGER NOT NULL, "
+						+ "styling VARCHAR(7) NOT NULL, "
+						+ "type VARCHAR(7) NOT NULL, "
+						+ "content CLOB NOT NULL, "
+						+ "PRIMARY KEY (page_id, index), "
+						+ "CHECK (styling='airy' OR styling='compact') "
 					+ ")",
 		};
 		
@@ -119,19 +130,21 @@ public class SchemaDefinition {
 	}
 	
 	public static String[] getDropTableQueries(String prefix){
+		//TODO validate prefix
+		
 		String[] queries = {
-				"DROP TABLE user",
-				"DROP TABLE message",
-				"DROP TABLE receives",
-				"DROP TABLE alert",
-				"DROP TABLE notifies",
-				"DROP TABLE page",
-				"DROP TABLE viewable_by",
-				"DROP TABLE comment",
-				"DROP TABLE menu_item",
-				"DROP TABLE menu_item_configuration",
-				"DROP TABLE page_menu",
-				"DROP TABLE content",
+				"DROP TABLE " + prefix + "user",
+				"DROP TABLE " + prefix + "message",
+				"DROP TABLE " + prefix + "receives",
+				"DROP TABLE " + prefix + "alert",
+				"DROP TABLE " + prefix + "notifies",
+				"DROP TABLE " + prefix + "page",
+				"DROP TABLE " + prefix + "viewable_by",
+				"DROP TABLE " + prefix + "comment",
+				"DROP TABLE " + prefix + "menu_item",
+				"DROP TABLE " + prefix + "menu_item_configuration",
+				"DROP TABLE " + prefix + "page_menu",
+				"DROP TABLE " + prefix + "content",
 		};
 		
 		return queries;
@@ -155,6 +168,8 @@ public class SchemaDefinition {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		w.println("connect to cs421;");//connect to db at the top of the script.
 		
 		for(String query: queries){
 			w.println(query + ";");
